7d1952d6635c526965a149f749ba629fe1d1fd5f
Thuan Van Bao Phung <thuanphung3012@Thuans-MBP.lan>
2020-12-07 10:01:26 -0500

    update homepage

--- js/main.js
+++ js/main.js
@@ -48,4 +48,34 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Button "more information" - "less information"
+$('button').on("click", function() {
+  $(this).toggleClass('active');
+});
+
+10
+
+$(document).ready(function () {
+    var expand = $("#Expand");
+    expand.cliked = 1;
+    expand.click(function () {
+        $("#expand_p").text((expand.cliked++ % 2 == 0) ? "More information" : "Less information");
+    });
+});
   
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active2");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
